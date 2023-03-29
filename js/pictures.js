3cd1f4aed5e1e51fdaca75e7dab90e7ea91b2f71
oleg <s1ckoleg@mail.ru>
2023-03-30 01:54:56 +0300

    implement pictures adding

---
+++ js/pictures.js
@@ -0,0 +1,30 @@
+const pictureTemplate = document.querySelector('#picture');
+
+let generatePicturesFromObject = function (object) {
+  const picture = document.createElement('div');
+  const pictureClone = pictureTemplate.content.cloneNode(true);
+  picture.classList.add('picture');
+  picture.append(pictureClone);
+
+  const pictureImage = picture.querySelector('.picture__img');
+  const pictureComments = picture.querySelector('.picture__comments');
+  const pictureLikes = picture.querySelector('.picture__likes');
+
+  pictureImage.src = object.url;
+  pictureImage.alt = object.description;
+  pictureComments.textContent = object.comments;
+  pictureLikes.textContent = object.likes;
+
+  return picture;
+}
+
+
+let showPictures = function (objects) {
+  const fragment = new DocumentFragment();
+  picturesData.forEach((object) => {
+    fragment.append(generatePicturesFromObject(objects));
+  });
+  document.querySelector('.pictures').append(fragment);
+}
+
+export { showPictures }
